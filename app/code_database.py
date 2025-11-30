fc8aec572cb1b9ec4bca9dd837ef01f8eaa39e83
0x4kgi <17591127+0x4kgi@users.noreply.github.com>
2025-11-30 15:40:41 +0800

    Move away from pantry to local json files (finally)

--- app/code_database.py
+++ app/code_database.py
@@ -1,43 +1,34 @@
-import requests
 import json
-import os
-from dotenv import load_dotenv
+import sys
 
-load_dotenv()
+def get_json(basket):
+    try:
+        with open(f'codes/{basket}.json', 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print(f'.json not found: codes/{basket}.json')
+        return None
+    
+    return data
 
-PANTRY_KEY = os.getenv('PANTRY_KEY')
 
-# this is a bad idea but i kinda dont care since we are only using one
-# pantry anyway
-def basket_request(method='GET', payload='', basket=None):
-    # basket names will always have "codes-" prefix
-    # "codes" is the legacy basket
-    BASKET_URL = f'https://getpantry.cloud/apiv1/pantry/{PANTRY_KEY}/basket/codes'
-
-    if basket is not None:
-        BASKET_URL += '-' + basket
-
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    return requests.request(
-        method=method, 
-        url=BASKET_URL,
-        headers=headers, 
-        data=payload
-    )
+def save_json(payload='', basket=None):
+    try:
+        with open(f'codes/{basket}.json', 'w') as file:
+            file.write(payload)
+    except:
+        print(f'Something went wrong with writing to codes/{basket}.json')
 
 
 def get_basket(key=None, basket=None):
     print(f'[{basket} : {key}] Fetching pantry basket...')
-    response = basket_request(method='GET', basket=basket)
+
+    data = get_json(basket)
     
-    if not response:
+    if not data:
         print(f'Empty basket: {basket}')
         return None
     
-    data = json.loads(response.text)
-
     if key is None:
         return data
 
@@ -54,11 +45,11 @@ def get_saved_codes(game=None):
 
 def update_basket(data, basket=None):
     payload = json.dumps(data)
-    basket_request(method='POST', payload=payload, basket=basket)
+    save_json(payload=payload, basket=basket)
 
 
 def update_saved_codes(codes, game=None):
-    print(f'[{game}] Updating saved codes to pantry...')
+    print(f'[{game}] Updating saved codes to json...')
     update_basket(
         data={
         'codes': codes
@@ -67,11 +58,20 @@ def update_saved_codes(codes, game=None):
 
 
 if __name__ == '__main__':
-    print('running as main')
-
-    print(get_basket(key='ayaya', basket='some-game'))
-
-    stuff = {'stuff': 'lmao'}
-    update_basket(data=stuff, basket='some-game')
-
-    print(get_basket(basket='some-game'))
+    args = sys.argv[1:]
+    
+    if len(args) < 1:
+        n = get_json(None)
+        c = get_json('c')
+        
+        print(n)
+        print(c)
+    elif args[0] == 'write-test' and args[1] is not None:
+        print(f'writing to {args[1]}.json')
+        update_basket({
+            'key': 'value', 'some-key': 69, 'other': False
+        }, args[1])
+        print(get_basket(basket=args[1], key='other'))
+    else:
+        for arg in args:
+            print(get_json(arg))
